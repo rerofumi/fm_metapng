d80831aec031f860eab2a5fdf840444aeda73fdc
rerofumi <rero2@yuumu.org>
2025-02-09 00:28:03 +0900

    Enter 待ちしてから終了する機能を追加

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use clap::Parser;
 use png::Decoder;
 use std::fs::File;
+use std::io;
+use std::io::prelude::*;
 
 /// fm_metapng
 #[derive(Parser, Debug)]
@@ -8,11 +10,14 @@ use std::fs::File;
 struct Args {
     /// PNG file path
     path: String,
+
+    /// Do not pause before exiting
+    #[arg(short = 'n', long = "no-pause")]
+    no_pause: bool,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let path = std::env::args().nth(1).expect("Please provide a path to the PNG file.");
-    let args = Args { path };
+    let args = Args::parse();
 
     let file = File::open(args.path)?;
     let decoder = Decoder::new(file);
@@ -22,14 +27,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut displayed = false;
     for text in info.uncompressed_latin1_text.iter() {
         if text.keyword == "parameters" {
-            let prompt = text.text.replace("Negative prompt: ", "\nNegative prompt:\n");
+            let prompt = text.text.replace("Negative prompt: ", "\nNegative prompt: \n");
             println!("Positive prompt:");
             println!("{}", prompt);
             displayed = true;
         }
-    }    
+    }
     if !displayed {
         println!("prompt info is not found.");
+    }
+
+    if !args.no_pause {
+        println!("Press Enter to exit...");
+        let mut stdin = io::stdin();
+        let mut stdout = io::stdout();
+
+        // We want the cursor to stay at the end of the line, so we print without a newline and flush manually.
+        write!(stdout, "").unwrap();
+        stdout.flush().unwrap();
+
+        // Read a single byte and discard
+        let _ = stdin.read(&mut [0u8]).unwrap();
     }
 
     Ok(())
